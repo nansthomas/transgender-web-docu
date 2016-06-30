9270dd8bb4d2035308b8b80c954f3ecfdc281144
Nans THOMAS <nans.thomas@hetic.net>
2016-06-30 14:01:54 +0200

    Fix the Scroll when Menu is Open ! 🐪

--- assets/js/app.js
+++ assets/js/app.js
@@ -120,22 +120,70 @@ twitterClose.addEventListener('click', function () {
   twitterButton.style.display = 'block';
 });
 
-// MENU ANIMATION
+// DISABLE SCROLL
 
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+    e.preventDefault();
+  e.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+    window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+  if (window.removeEventListener)
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.onmousewheel = document.onmousewheel = null;
+  window.onwheel = null;
+  window.ontouchmove = null;
+  document.onkeydown = null;
+}
+
+// MENU ANIMATION
+var body = document.querySelector('body');
 var menuButton = document.querySelector('#menu-circle');
 var openMenuIcon = document.querySelector('#open-menu');
 var closeMenuIcon = document.querySelector('#close-menu');
-var menuDeploy = document.querySelector('#menu-deploy');
+var menuDeploy = document.querySelector('#menu-content');
 
+// Open Menu
 openMenuIcon.addEventListener('click', function () {
+  body.style.overflow = 'hidden';
+  $.scrollify.disable();
   menuButton.style.transform = 'scale(80)';
   openMenuIcon.style.display = 'none';
   closeMenuIcon.style.display = 'block';
+  setTimeout(function () {
+    menuDeploy.style.display = 'flex';
+  }, 350);
 });
 
+// Close Menu
 closeMenuIcon.addEventListener('click', function () {
+  body.style.overflow = 'auto';
+  $.scrollify.enable();
   menuButton.style.transform = 'scale(1)';
   closeMenuIcon.style.display = 'none';
+  menuDeploy.style.display = 'none';
   setTimeout(function () {
     openMenuIcon.style.display = 'block';
   }, 300);
